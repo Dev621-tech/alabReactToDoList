05b6d3b5a8ce8ed5d541fc7206f172b8811517c5
Dev621-tech <devinosborne11@yahoo.com>
2026-02-20 17:07:27 -0500

    Added CSS

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -32,10 +32,12 @@ export default function ToDo() {
     }
 
     function deleteTodo(id) {
+        // Filter and delete the one that matches the id
         setTodos(todos.filter((todo) => todo.id !== id))
     }
 
     function enableEdit(id) {
+        // Changes the one with the matching ID property isEditing to true
         setTodos(
             todos.map((todo) =>
                 todo.id === id ? { ...todo, isEditing: true } : todo
@@ -44,6 +46,7 @@ export default function ToDo() {
     }
 
     function handleEditChange(id, value) {
+        // Find the matching id and set the new value for the text property
         setTodos(
             todos.map((todo) =>
                 todo.id === id ? { ...todo, text: value } : todo
@@ -52,6 +55,7 @@ export default function ToDo() {
     }
 
     function saveEdit(id, newText) {
+        // set the changes and changes isEditing to false
         setTodos(
             todos.map((todo) =>
                 todo.id === id ? { ...todo, text: newText, isEditing: false } : todo
@@ -59,10 +63,21 @@ export default function ToDo() {
         );
     }
 
+    const style = {
+    maxWidth: "500px",
+    margin: "20px auto",
+    padding: "20px",
+    border: "4px groove rebeccapurple",
+    borderRadius: "50px",
+    boxShadow: "20px 20px 20px rgba(0,0,0,0.1)",
+    fontFamily: "Verdana, sans-serif",
+    backgroundColor: "#ccc"
+    }
+
 
     return (
         <>
-            <div>
+            <div style={style}>
                 <h1>ToDo List</h1>
                 <input
                     type="text"
